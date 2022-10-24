923c4a1abe25cd2ad182b6a74ba20219d5340852
Robson <41270745+ROSS1996@users.noreply.github.com>
2022-10-24 08:08:21 -0300

    Fix contiguous calculation

--- calculator.js
+++ calculator.js
@@ -4,10 +4,10 @@ let cc = document.getElementById('cc')
 let operators = document.getElementsByClassName('operator')
 let calculate = document.getElementById('calc')
 
-let number1 = 0;
-let number2 = 0;
-let lastResult = undefined;
-let continueCalc = false;
+let number1 = undefined;
+let number2 = undefined;
+
+let answer = undefined;
 
 let operation = undefined
 let currentNumber = ''
@@ -18,16 +18,14 @@ calculate.addEventListener('click', function (){
 
 for (const btn of numbers) {
     btn.addEventListener('click', function (){
-        if (continueCalc == true) {
-            clearElements(1, true, false);
-            changeNumber(this.id, 2)
-        }
-        else if (operation == undefined) {
+        if (operation == undefined) {
+            if (answer != undefined ) {
+                clearElements(3, true);
+                clearMemory(3, true, true);
+            }
             changeNumber(this.id, 1)
         }
-        else {
-            console.log(`Continue calc: ${continueCalc}`)
-            console.log(`Operation: ${operation}`)
+        else if (number1 !== undefined && operation !== undefined) {
             changeNumber(this.id, 2)
         }
     })
@@ -35,17 +33,16 @@ for (const btn of numbers) {
 
 for (const btn of operators) {
     btn.addEventListener('click', function (){
-        if (lastResult !== undefined) {
-            clearElements(1, false, false);
-            changeOperation(this.id);
-            console.log(`Last result: ${operation}}`)
-            changeNumber(lastResult, 1);
+        if (number1 !== undefined) {
+            changeOperation(this.id)
         }
-        changeOperation(this.id);
     }
 )}
 
-cc.addEventListener('click', function () { clearElements (3, true, true);})
+cc.addEventListener('click', function () {
+    clearElements(3, true);
+    clearMemory(3, true, true);
+})
 
 function changeNumber (number, position) {
     if (position == 1) {
@@ -53,7 +50,8 @@ function changeNumber (number, position) {
         currentNumber = currentNumber.concat(number)
         number1 = parseInt(currentNumber);
         input.innerText = number1;
-    } else {
+    }
+    else if (position == 2) {
         let input = document.getElementById('number2')
         currentNumber = currentNumber.concat(number)
         number2 = parseInt(currentNumber);
@@ -95,33 +93,44 @@ function operate () {
         result = subtract(number1, number2)
     }
     input.innerText = result;
-    lastResult = result;
-    clearElements(0, false, true);
+    answer = result;
+    clearElements(2);
+    clearMemory(2, true);
+    changeNumber(result, 1)
 }
 
-function clearElements (elements, memory = false, operation = false) {
+function clearElements (input, result = false) {
     let element = document.getElementsByClassName('screeElement');
-    [number1, number2] = [0,0];
+    if (input == 1) {
+        element[0].innerText = '';
+    } else if (input == 2) {
+        element[1].innerText = '';
+        element[2].innerText = '';
+    }
+    else if (input == 3) {
+        for (i = 0; i < 3; i++) { element[i].innerText = ''};
+    }
+    if (result == true) {
+        element[3].innerText = 0;
+    }
+}
+
+function clearMemory (number, op = false, ans = false) {
     currentNumber = '';
-    if (elements == 1) {
-        for (i = 0; i < 3; i++) { element[i].innerText = ''};
+    if (number == 1) {
+        number1 = undefined
     }
-    else if (elements == 2) {
-        for (i = 0; i < 3; i++) { element[i].innerText = ''};
-        element[3].innerText = 0;
+    else if (number == 2) {
+        number2 = undefined
     }
-    else if (elements == 3) {
-        for (i = 0; i < 3; i++) { element[i].innerText = ''};
-        element[3].innerText = 0;
+    else if (number == 3) {
+        [number1, number2] = [undefined,undefined];
     }
-    if (memory == true) {
-        lastResult = undefined;
-        continueCalc = false;
-    }
-    if (operation == true) {
-        console.log('limpei operation?')
+    if (op == true ) {
         operation = undefined;
-        console.log(`Operation ${operation}`)
+    }
+    if (ans == true) {
+        answer = undefined
     }
 }
 
@@ -129,6 +138,9 @@ function clearElements (elements, memory = false, operation = false) {
 function add (a, b) { return a + b};
 function subtract (a, b) {return a - b};
 function multiply (a, b) {return a * b};
-function divide (a, b) {return a / b};
-
-
+function divide (a, b) {
+    if (a === 0 || b === 0) {
+        return 'Infinity'
+    }
+    return a / b
+};
